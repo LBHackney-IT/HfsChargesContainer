4227ac59a653b45836f298e447ddc701ed921b61
duslerke <43747286+Duslerke@users.noreply.github.com>
2023-08-04 17:15:23 +0100

    Remove the redaction from within the connection string. Won't add the log of connstr as it's already confirmed the env vars get picked up.

--- HfsChargesContainer/Program.cs
+++ HfsChargesContainer/Program.cs
@@ -1,4 +1,4 @@
-﻿using HfsChargesContainer;
+using HfsChargesContainer;
 using HfsChargesContainer.Gateways;
 using HfsChargesContainer.Gateways.Interfaces;
 using HfsChargesContainer.Infrastructure;
@@ -15,7 +15,7 @@ string dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? throw new Argum
 string dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? throw new ArgumentNullException(nameof(dbUser));
 string dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? throw new ArgumentNullException(nameof(dbPassword));
 
-string connectionString = $"Data Source={dbHost},1433;Initial Catalog={dbName};Integrated Security=False;User Id={dbUser};Password=**redacted**;Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=True;";
+string connectionString = $"Data Source={dbHost},1433;Initial Catalog={dbName};Integrated Security=False;User Id={dbUser};Password={dbPassword};Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=True;";
 
 var serviceProvider = new ServiceCollection()
     .AddDbContext<DatabaseContext>(opt =>
