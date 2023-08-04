d9dec5db8e94aa9e2d38861fe891060b527a0aa3
duslerke <43747286+Duslerke@users.noreply.github.com>
2023-08-04 13:19:51 +0100

    Configure local environment variable pick-up.
    Printing it to Console to verify that it works.

--- HfsChargesContainer/Program.cs
+++ HfsChargesContainer/Program.cs
@@ -5,12 +5,19 @@ using Microsoft.Extensions.DependencyInjection;
 
 Console.WriteLine("Application started!");
 
+Console.WriteLine("Configuring the Start up!");
+string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? throw new ArgumentNullException(nameof(connectionString));
+
+Console.WriteLine(connectionString);
+
 var serviceProvider = new ServiceCollection()
     .AddScoped<IUseCase1, UseCase1>()
     .AddScoped<ProcessEntryPoint>()
     .BuildServiceProvider();
 
 var entryPoint = serviceProvider.GetRequiredService<ProcessEntryPoint>();
+
+Console.WriteLine("Ready to Run!");
 entryPoint.Run();
 
 Console.WriteLine("Application finished!");
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # HfsChargesContainer
 EC2 container for Housing Finance System "Charges Import" Nightly Process.
+
+# Running
+This application is supposed to run as docker container.
+1. Create a copy of `.env.sample` and then populated with the required values.
+    ``` sh
+    cp .env.sample .env
+    ```
+2. Build and run the application docker container using a Makefile
+    ``` sh
+    make run
+    ```
+
+// TODO: more steps to come as more things get configured.
+ 
