9dac59594ad0b52aeecdb2e92664ee30c4709202
duslerke <43747286+Duslerke@users.noreply.github.com>
2023-08-04 16:37:20 +0100

    Removed some log sfor environment variable checks.

--- HfsChargesContainer/Program.cs
+++ HfsChargesContainer/Program.cs
@@ -15,9 +15,7 @@ string dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? throw new Argum
 string dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? throw new ArgumentNullException(nameof(dbUser));
 string dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? throw new ArgumentNullException(nameof(dbPassword));
 
-string connectionString = $"Data Source={dbHost},1433;Initial Catalog={dbName};Integrated Security=False;User Id={dbUser};Password={dbPassword};Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=True;";
-
-Console.WriteLine(connectionString);
+string connectionString = $"Data Source={dbHost},1433;Initial Catalog={dbName};Integrated Security=False;User Id={dbUser};Password=**redacted**;Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=True;";
 
 var serviceProvider = new ServiceCollection()
     .AddDbContext<DatabaseContext>(opt =>
