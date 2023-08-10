d0be5706b3163290b68a1a7bbaef7eab78bb6ed3
duslerke <43747286+Duslerke@users.noreply.github.com>
2023-08-10 15:40:15 +0100

    Add the 'ReadSheetToEntities' method to the GoogleClientService.

--- HfsChargesContainer/Gateways/GoogleClientService.cs
+++ HfsChargesContainer/Gateways/GoogleClientService.cs
@@ -2,6 +2,7 @@ using System.Dynamic;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 using HfsChargesContainer.Gateways.Interfaces;
+using HfsChargesContainer.Helpers;
 using Newtonsoft.Json;
 
 namespace HfsChargesContainer.Gateways
@@ -56,6 +57,69 @@ namespace HfsChargesContainer.Gateways
             }
 
             return JsonConvert.SerializeObject(rowObjects);
+        }
+
+        public async Task<IList<_TEntity>> ReadSheetToEntitiesAsync<_TEntity>(string spreadSheetId, string sheetName,
+            string sheetRange) where _TEntity : class
+        {
+            SpreadsheetsResource.ValuesResource.GetRequest getter =
+                _sheetsService.Spreadsheets.Values.Get(spreadSheetId, $"{sheetName}!{sheetRange}");
+
+            ValueRange response = await getter.ExecuteAsync().ConfigureAwait(false);
+            IList<IList<object>> values = response.Values;
+
+            if (values == null || !values.Any())
+            {
+                LoggingHandler.LogInfo($"No data found. Spreadsheet id: {spreadSheetId}, sheet name: {sheetName}, sheet range: {sheetRange}");
+                return null;
+            }
+            LoggingHandler.LogInfo($"Rows {values.Count} found");
+
+            // Get the headers
+            IList<string> headers = values.First().Select(cell => cell.ToString()).ToList();
+            IList<object> rowObjects = new List<object>();
+            LoggingHandler.LogInfo($"Writing row values to objects, {headers.Count} headers found");
+
+            // For each row of actual data
+            foreach (var row in values.Skip(1))
+            {
+                int cellIterator = 0;
+                int rowCellCount = row.Count;
+                dynamic rowItem = new ExpandoObject();
+                var rowItemAccessor = rowItem as IDictionary<string, object>;
+
+                // Add the cell values using the headers as properties
+                foreach (string header in headers)
+                {
+                    if (cellIterator < rowCellCount)
+                    {
+                        string propertyName = string.IsNullOrWhiteSpace(header)
+                            ? $"Header{cellIterator}"
+                            : header;
+
+                        // Assign the value to this property
+                        rowItemAccessor[propertyName] = row[cellIterator++];
+                    }
+                }
+                rowObjects.Add(rowItem);
+            }
+
+
+            try
+            {
+                LoggingHandler.LogInfo($"Writing values to objects and serializing");
+                string convertedJson = JsonConvert.SerializeObject(rowObjects);
+                var entities = JsonConvert.DeserializeObject<IList<_TEntity>>(convertedJson);
+
+                return entities;
+            }
+            catch (Exception exc)
+            {
+                LoggingHandler.LogInfo($"Error writing values to objects and serializing");
+                LoggingHandler.LogInfo(exc.ToString());
+
+                throw;
+            }
         }
     }
 }
--- HfsChargesContainer/Gateways/Interfaces/IGoogleClientService.cs
+++ HfsChargesContainer/Gateways/Interfaces/IGoogleClientService.cs
@@ -1,9 +1,10 @@
-using System.Threading.Tasks;
-
 namespace HfsChargesContainer.Gateways.Interfaces
 {
     public interface IGoogleClientService
     {
         public Task<string> ReadSheetAsync(string spreadSheetId, string sheetName, string sheetRange);
+
+        public Task<IList<_TEntity>> ReadSheetToEntitiesAsync<_TEntity>(string spreadSheetId, string sheetName,
+            string sheetRange) where _TEntity : class;
     }
 }
