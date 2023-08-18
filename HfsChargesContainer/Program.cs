12bef5fa819b91441c1f3f51d04bc28e48c953a2
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2023-08-18 14:05:54 +0100

    test sns access

--- HfsChargesContainer/Program.cs
+++ HfsChargesContainer/Program.cs
@@ -1,14 +1,43 @@
 ﻿using HfsChargesContainer;
+using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
 
 Console.WriteLine("Application started!\nConfiguring the Start up!");
 
-var entryPoint = new Startup()
-    .ConfigureServices()
-    .Build<ProcessEntryPoint>();
+// var entryPoint = new Startup()
+//     .ConfigureServices()
+//     .Build<ProcessEntryPoint>();
 
-Console.WriteLine("Ready to Run!");
-await entryPoint.Run();
+// Console.WriteLine("Ready to Run!");
+// await entryPoint.Run();
 
-Console.Error.WriteLine("Test Error Log Behaviour");
+// Console.Error.WriteLine("Test Error Log Behaviour");
+Console.WriteLine(Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID")?.Substring(0,4));
+
+string topicId = Environment.GetEnvironmentVariable("TOPIC_ARN");
+
+Console.WriteLine(topicId?.Substring(0,7));
+
+var clientConfig = new AmazonSimpleNotificationServiceConfig()
+{
+    RegionEndpoint = RegionEndpoint.EUWest2,
+};
+
+
+var asnsClient = new AmazonSimpleNotificationServiceClient(clientConfig);
+
+string topicArn = topicId;
+string messageText = @"Unhandled exception. Amazon.SimpleNotificationService.Model.InvalidParameterException: Invalid parameter: TopicArn
+ ---> Amazon.Runtime.Internal.HttpErrorResponseException: Exception of type 'Amazon.Runtime.Internal.HttpErrorResponseException' was thrown.";
+
+var request = new PublishRequest
+{
+    TopicArn = topicArn,
+    Message = messageText,
+    Subject = "Test subject"
+};
+
+await asnsClient.PublishAsync(request).ConfigureAwait(false);
 
 Console.WriteLine("Application finished!");
