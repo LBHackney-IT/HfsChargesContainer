6d1c91d4c786aa93be31022487a4436808571144
Adam <74552077+adamtry@users.noreply.github.com>
2026-04-10 12:32:23 +0100

    Only log process sucess at the end of the last process (#35)

--- HfsChargesContainer/UseCases/LoadChargesTransactionsUseCase.cs
+++ HfsChargesContainer/UseCases/LoadChargesTransactionsUseCase.cs
@@ -41,7 +41,7 @@ namespace HfsChargesContainer.UseCases
                 await _chargesBatchYearsGateway.SetToSuccessAsync(pendingYear.Year).ConfigureAwait(false);
                 await _batchLogGateway.SetToSuccessAsync(batch.Id).ConfigureAwait(false);
                 LoggingHandler.LogInfo($"End charges transactions import");
-                LoggingHandler.LogInfo(LoggingHandler.ProcessCompletedSuccessfullyMessage);
+                LoggingHandler.LogInfo("Process completed successfully");
             }
             catch (Exception exc)
             {
